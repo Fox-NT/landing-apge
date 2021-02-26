f3f7f7ffc5a9373b88ac4dc37299fc7a5b06fef1
Fox-NT <newtorka@gmail.com>
2021-02-26 13:47:20 +0600

    burger

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-"use strict" 
+"use strict"
 
 document.addEventListener('DOMContentLoaded', () => {
 
 const burger = document.querySelector('.burger'),
-        menu = document.querySelector('.mobile_wrapper');
+        menu = document.querySelector('.mobile_wrapper'),
+        body = document.querySelector('body');
 
         burger.addEventListener('click', () => {
             menu.classList.toggle('visible_mobile');
             burger.classList.toggle('burger_active');
+            body.classList.toggle('over');
         });
 
 });
