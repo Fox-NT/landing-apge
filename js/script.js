21ab939dd3f67f00b4c9a785f1924a9d924e8554
Fox-NT <newtorka@gmail.com>
2021-02-26 15:14:17 +0600

    test

--- js/script.js
+++ js/script.js
@@ -4,12 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const burger = document.querySelector('.burger'),
         menu = document.querySelector('.mobile_wrapper'),
-        body = document.querySelector('body');
+        body = document.querySelector('body'),
+        list = document.querySelector('.nav_list'),
+        lang = document.querySelector('.language_mobile');
 
         burger.addEventListener('click', () => {
             menu.classList.toggle('visible_mobile');
             burger.classList.toggle('burger_active');
             body.classList.toggle('over');
+            list.classList.toggle('nav_list_active');
+            lang.classList.toggle('lang_mobile_activ');
         });
 
 });
